a9e376fbeffed9a78baa7fd6844ed59c12c90bc9
Patryk Kowalski <patrykkowalski@Patryks-iMac.local>
2019-06-03 15:26:51 +0200

    cleaning boilerplates

--- src/commonJS-training/map.js
+++ src/commonJS-training/map.js
@@ -1,12 +1,5 @@
 const shoppingItems = require('./object_information');
 
-const newItem = {
-    name: 'pear',
-    price: 0.99,
-    category: 'food',
-    quantity: 2
-};
-
 const mapThroughItems = shoppingItems.map(({ name, price, quantity}) => {
     return `${ name } | $${ price } | ${quantity}`
 });
