5f7c898dd69dddc257772e17e2e65e76cc0f5d7b
kowalus23 <kowalus23@gmail.com>
2019-06-06 13:51:10 +0200

    example updates in js file

---
+++ src/commonJS-training/simple_methods.js
@@ -0,0 +1,18 @@
+// slice
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const newArray = myArray.slice(3, 6);
+
+
+console.log(newArray);
+
+// splice
+const my2Array =  [1, 10, 20, 30, 40, 50];
+const cutBeforeIndex=  my2Array.splice(3);
+
+console.log(my2Array); // mutated my2Array after splice
+console.log(`we are a cut of rest of my2Array ${cutBeforeIndex}`);
+
+// forEach
+
+const animals = ['dog', 'cat', 'bear', 'bird', 'pig'];
+animals.forEach((element, index) => console.log(`(${element}) is at ${index} index`));
